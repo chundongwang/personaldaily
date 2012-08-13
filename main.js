480d3460e1ad40474d65022cc65d675371db9bf8
CD.Wang <jewelry_wolf@hotmail.com>
2012-08-13 16:17:49 -0700

    Empty shim of webserver
    Nodejs-based web server with the ability to pick port according to
    computer name, favor for developers behind NAT.

---
+++ main.js
@@ -0,0 +1,23 @@
+var http = require('http');
+var port_table = [['JEWELRY-MSFT', 8080]]
+
+var port = 80;
+for (var i=0; i<port_table.length; i++) {
+  console.info('looping computer in port table: '+global.process.env.COMPUTERNAME);
+  if (port_table[i][0]==global.process.env.COMPUTERNAME) {
+    port = port_table[i][1];
+    break;
+  }
+}
+
+if (port == 80) {
+  console.warn('Cannot find this computer in port table. You might have problem with NAT!!');
+}
+
+http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.end('Hello World\n');
+}).listen(port, '127.0.0.1');
+
+console.log('Server running at http://127.0.0.1:%d/', port);
+console.log('Go to http://192.168.1.1/ to add port penetration config with your IP address and port for HTTP.');
